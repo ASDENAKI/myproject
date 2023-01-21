2255aeeecf824fe70474fda1512102647b94b224
sophoniyas <114131250+sophoniyas@users.noreply.github.com>
2023-01-21 11:18:59 -0500

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,6 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - This is printf function
  * @format: format.
+ * authors: sophoniyas and asdenaki
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
