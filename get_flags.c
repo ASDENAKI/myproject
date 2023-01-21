7f5db88bfb0877994197235326e469e23ff36502
sophoniyas <114131250+sophoniyas@users.noreply.github.com>
2023-01-21 11:28:18 -0500

    Create get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -4,6 +4,7 @@
  * get_flags - Calculate active flags
  * @format: Formatted string in which to print the arguments
  * @i: take a parameter.
+ * authors: sophoniyas and asdenaki
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
